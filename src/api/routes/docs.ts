978327205db4f45fb0a86d0fdaa9b43dde76bf0e
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2020-02-20 10:54:58 +0100

    Move Swagger config to separate file

--- src/api/routes/docs.ts
+++ src/api/routes/docs.ts
@@ -1,21 +1,11 @@
 import { Router } from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
+import swaggerOptions from "../../../swagger.json";
 
 const router = Router();
 
-const options = {
-  definition: {
-    info: {
-      title: "Hello World",
-      version: "1.0.0",
-      description: "An example API"
-    }
-  },
-  apis: ["src/**/*.ts"]
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 router.use(swaggerUI.serve);
 router.get("/", swaggerUI.setup(swaggerSpec));
