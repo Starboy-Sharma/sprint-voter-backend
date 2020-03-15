0aef1fd3db8f6c59fd3cf0a9b02771e4a7d27e1e
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2020-03-15 21:17:52 +0100

    Replace openapi-jsdoc with swagger-jsdoc

--- src/api/routes/docs.ts
+++ src/api/routes/docs.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-import openApiJSDoc from "openapi-jsdoc";
 import redoc from "redoc-express";
+import createSpecFromJSDoc from "swagger-jsdoc";
 
-import swaggerOptions from "../../config/openapi.json";
+import options from "../../config/openapi.json";
 
 const router = Router();
 
-const spec = openApiJSDoc(swaggerOptions);
+const spec = createSpecFromJSDoc(options);
+const title = options.definition.info.title;
+
 router.get("/spec.json", (_, res) => res.json(spec));
-router.get("/", redoc({ title: spec.info.title, specUrl: "docs/spec.json" }));
+router.get("/", redoc({ title, specUrl: "docs/spec.json" }));
 
 export default router;
