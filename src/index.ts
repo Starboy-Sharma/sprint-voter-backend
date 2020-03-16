f391d419e22b2f9d9b02d4e7fc49bc0e15ecdb76
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2020-03-16 12:21:09 +0100

    Setup loader for listeners
    Automatically imports all files in src/listeners.
    Individual files can "subscribe" themselves.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import config from "./config";
 
-import mongooseLoader from "./loaders/mongoose";
+import loadMongoose from "./loaders/mongoose";
+import loadListeners from "./loaders/listeners";
 
 async function start(): Promise<void> {
-  await mongooseLoader();
+  await loadMongoose();
+  await loadListeners();
 
   app.listen(config.app.port, () => {
     console.log(`App running on port ${config.app.port}`);
---
+++ src/loaders/listeners.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+import * as path from "path";
+
+function readdir(path: fs.PathLike): Promise<string[] | Buffer[]> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) return reject(err);
+      else resolve(files);
+    });
+  });
+}
+
+// Dynamically import all files under the "/listeners" directory.
+// This way we don't have to import each file individually.
+export default async function(): Promise<void> {
+  const pathToListeners = path.join(__dirname, "../listeners");
+  try {
+    const files = await readdir(pathToListeners);
+    for (const file of files) {
+      if (file.constructor.name === "String") {
+        const fileName = file as string;
+        import(`../listeners/${fileName}`);
+      }
+    }
+  } catch (error) {
+    console.error("Problem loading listeners:");
+    console.error(error.toString());
+    process.exit(1);
+  }
+}
