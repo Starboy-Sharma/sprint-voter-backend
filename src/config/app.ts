47864825c5c9ea3584ed43be05b16421c27e1351
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2020-03-16 17:00:37 +0100

    Add debug getter to app config
    Shorthand for `env !== "production".

--- src/config/app.ts
+++ src/config/app.ts
@@ -9,6 +9,12 @@ type AppConfig = {
   env: string;
 
   /**
+   * This value determines if the application is in debug mode. It is a shorthand
+   * which is based on the "environment", see {@link AppConfig.env}.
+   */
+  debug: boolean;
+
+  /**
    * This value determines the port on which the application will listen.
    *
    * @default 8000
@@ -18,6 +24,9 @@ type AppConfig = {
 
 const appConfig: Readonly<AppConfig> = {
   env: process.env.NODE_ENV || "production",
+  get debug() {
+    return this.env !== "production";
+  },
   port: Number(process.env.PORT) || 8000
 };
 
