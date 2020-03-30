6916029d03aaf27f8bb83dc114ffe4a358227631
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2020-03-30 08:51:41 +0200

    Setup request validation including error handler using 'express-validation'

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
+import { ValidationError } from "express-validation";
 
 import config from "./config";
 import { stream } from "./logger";
@@ -18,5 +19,13 @@ app.use("/items", itemsRouter);
 if (config.app.env === "development") {
   app.use("/docs", docsRouter);
 }
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err);
+  } else {
+    return res.status(500).json(err);
+  }
+});
 
 export default app;
