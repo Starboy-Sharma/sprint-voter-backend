a798a590422d888bdfca5b847e44ac27d914fc34
Pankaj Sharma <pankaj.sharma@emids.com>
2022-10-22 14:14:25 +0530

    remove old code

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ...require("ts-jest/jest-preset"),
-  ...require("@shelf/jest-mongodb/jest-preset"),
-  coveragePathIgnorePatterns: [
-    "<rootDir>/tests/util/",
-    "<rootDir>/node_modules/"
-  ]
-};
--- src/events/base-event.ts
+++
@@ -1,8 +0,0 @@
-export default abstract class Event {
-  static get eventName(): string {
-    return this.name;
-  }
-  get eventName(): string {
-    return this.constructor.name;
-  }
-}
