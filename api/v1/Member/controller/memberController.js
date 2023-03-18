926953df8cb45c27727bc3fc06e4af05ebce8613
Pankaj Sharma <pankaj.sharma@emids.com>
2023-03-18 13:54:38 +0530

    create team members API #2

--- api/v1/Member/controller/memberController.js
+++ api/v1/Member/controller/memberController.js
@@ -1,36 +1,80 @@
+const { mongoose } = require('mongoose')
 const response = require('../../../../resonse/response')
+const teamModel = require('../../../../models/teams.model')
 
 class Member {
     static async addMembers(req, res) {
-        let { userIds } = req.body
+        try {
+            let { userIds } = req.body
+            const { teamId } = req.body
+            let result = {}
+            const userData = []
 
-        if (req.data.role !== 'team-manager') {
-            response.sendError({ errorCode: 'auth', error: '' }, res, 401)
-            return
-        }
+            if (req.data.role !== 'team-manager') {
+                response.sendError({ errorCode: 'auth', error: '' }, res, 401)
+                return
+            }
 
-        // make userIds unique
-        userIds = [...new Set(userIds)]
+            // make userIds unique
+            userIds = [...new Set(userIds)]
 
-        // userIds should not contain the login userId
-        if (userIds.includes(req.data.profileId)) {
-            response.sendError({ errorCode: 'apiStatus', error: '' }, res, 400)
-            return
-        }
+            // userIds should not contain the login userId
+            if (userIds.includes(req.data.profileId)) {
+                response.sendError(
+                    { errorCode: 'apiStatus', error: '' },
+                    res,
+                    400
+                )
+                return
+            }
 
-        console.log(userIds)
+            await Promise.all(
+                userIds.map(async (id) => {
+                    const isAlreadyMember = await teamModel.findOne({
+                        _id: mongoose.Types.ObjectId(teamId),
+                        'members.userId': mongoose.Types.ObjectId(id),
+                    })
 
-        // TODO Start add members database lookup code here.
-        response.sendSuccess(
-            {
-                result: {
-                    msg: 'Success Code',
-                    successCode: 'apiStatus',
+                    if (!isAlreadyMember) {
+                        const memberData = {
+                            userId: id,
+                            role: 'team-member',
+                        }
+
+                        userData.push(memberData)
+                    }
+                })
+            )
+
+            if (userData.length > 0) {
+                result = await teamModel.findOneAndUpdate(
+                    { _id: mongoose.Types.ObjectId(teamId) },
+                    {
+                        $addToSet: { members: { $each: userData } },
+                    },
+                    { new: true }
+                )
+            }
+
+            response.sendSuccess(
+                {
+                    result: {
+                        data: result,
+                        successCode: 'apiStatus',
+                    },
                 },
-            },
-            res,
-            200
-        )
+                res,
+                200
+            )
+        } catch (err) {
+            console.log('Add member error: ', err.message)
+
+            response.sendError(
+                { errorCode: 'serverError', error: err },
+                res,
+                500
+            )
+        }
     }
 }
 
