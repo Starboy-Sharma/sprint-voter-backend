261e4df1f80e55b200e59feb63a0987fac27fa2c
Pankaj Sharma <pankaj.sharma@emids.com>
2023-04-28 01:35:05 +0530

    init add user avatar url mongo migration with faker package

--- config/keys.js
+++ config/keys.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 module.exports = {
     MongoURI:
         process.env.MongoURI || 'mongodb://localhost:27017/sprint-planner',
+    DB: process.env.DATABASE || 'spring-planner',
 }
